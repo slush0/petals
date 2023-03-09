cd8dc7faec95b07ecd13ad38946d7d71100280c4
slush0 <marek@palatinus.cz>
2023-03-09 00:06:11 +0000

    Remove unnecessary code, fix logging to not confuser users.

--- src/petals/cli/convert_model_llama.py
+++ src/petals/cli/convert_model_llama.py
@@ -1,3 +1,12 @@
+"""
+    FIXME WIP, not working properly yet.
+
+    1. Use Transformers from https://github.com/zphang/transformers/tree/llama_push
+    2. Convert original LLaMA weights to Hugging Face format with
+           https://github.com/zphang/transformers/blob/llama_push/src/transformers/models/llama/convert_llama_weights_to_hf.py
+    3. Run: PETALS_IGNORE_DEPENDENCY_VERSION=1 python -m petals.cli.convert_model_llama --model llama-7b
+"""
+
 import argparse
 import os
 
@@ -18,50 +27,36 @@ logger = get_logger(__file__)
 
 DTYPE_MAP = dict(bfloat16=torch.bfloat16, float16=torch.float16, float32=torch.float32, auto="auto")
 
-# FIXME WIP, not working properly yet.
-#
-# 1. Use Transformers from https://github.com/zphang/transformers/tree/llama_push
-# 2. Convert original LLaMA weights to Hugging Face format with
-#       https://github.com/zphang/transformers/blob/llama_push/src/transformers/models/llama/convert_llama_weights_to_hf.py
-# 3. Run: PETALS_IGNORE_DEPENDENCY_VERSION=1 python -m petals.cli.convert_model_llama --model llama-7b --output_path ./llama-7b-petals
 
 def main():
     parser = argparse.ArgumentParser(description="Load bloom layers and convert to 8-bit using torch quantization.")
 
     parser.add_argument("--model", type=str, default="bigscience/bloom-6b3", help="Model name for from_pretrained")
-    parser.add_argument("--revision", type=str, default=None, help="Optional commit id from HF hub")
     parser.add_argument("--torch_dtype", type=str, default="auto", help="Load initial model in this dtype")
-    parser.add_argument("--output_path", type=str, default="./converted_model", help="Track output repo to this folder")
-    parser.add_argument("--output_repo", type=str, default="bigscience/test-bloomd", help="Push to this HF hub repo")
-    parser.add_argument("--client_branch", type=str, default=CLIENT_BRANCH, help="Save client version to this branch")
-    parser.add_argument(
-        "--block_branch_prefix", type=str, default=BLOCK_BRANCH_PREFIX, help="Save blocks to branches with this prefix"
-    )
-    parser.add_argument(
-        "--commit_message", type=str, default="push-o-matic", help="Use this commit message for all parts"
-    )
-    parser.add_argument("--use_auth_token", type=str, default=None, help="auth token for from_pretrained")
     parser.add_argument("--resize_token_embeddings", type=int, default=None, help="change the vocabulary size")
     args = parser.parse_args()
 
+    output_path = f"{args.model}-petals"
+
     free_ram_gb = psutil.virtual_memory().available / 2**30
-    if args.model == "bigscience/bloom" and free_ram_gb < 400:
-        logger.warning(f"ACHTUNG! converting bloom-176b will use up 350-400GB RAM, you have {free_ram_gb:.3f} free")
+    if args.model == "llama-65b" and free_ram_gb < 150:
+        # FIXME this is a guess
+        logger.warning(f"ACHTUNG! converting llama-65b will use up 150GB RAM, you have {free_ram_gb:.3f} free")
 
     assert args.torch_dtype in DTYPE_MAP, f"torch_dtype must be one of {list(DTYPE_MAP.keys())}"
-    if os.path.exists(args.output_path) and (
-        len(os.listdir(args.output_path)) != 0 or not os.path.isdir(args.output_path)
+    if os.path.exists(output_path) and (
+        len(os.listdir(output_path)) != 0 or not os.path.isdir(output_path)
     ):
         raise FileExistsError(f"Output path {args.output_path} already exists and is not an empty directory")
 
     logger.info(f"Loading source model {args.model} (this may take a few minutes)")
     config = DistributedLLaMAConfig.from_pretrained(
-        args.model, use_auth_token=args.use_auth_token, revision=args.revision
+        args.model,
     )
     config.dht_prefix = args.model #args.output_repo
 
     model = LLaMAModel.from_pretrained(
-        args.model, use_auth_token=args.use_auth_token, revision=args.revision, torch_dtype=DTYPE_MAP[args.torch_dtype]
+        args.model, torch_dtype=DTYPE_MAP[args.torch_dtype]
     )
     if args.resize_token_embeddings:
         logger.info(f"Resizing token embeddings, new size = {args.resize_token_embeddings}")
@@ -69,40 +64,30 @@ def main():
         config.vocab_size = args.resize_token_embeddings
 
     tokenizer = LLaMATokenizer.from_pretrained( #transformers.AutoTokenizer.from_pretrained(
-        args.model, use_auth_token=args.use_auth_token, revision=args.revision
+        args.model,
     )
-    os.makedirs(args.output_path, exist_ok=True)
-
-    #repo = Repository(args.output_path, clone_from=args.output_repo, use_auth_token=args.use_auth_token)
-    #repo.git_pull()
+    os.makedirs(output_path, exist_ok=True)
 
     #transformer_blocks = model.h
     transformer_blocks = model.decoder.layers
     logger.info(
-        f"Saving transformer blocks to {args.output_repo}@{args.block_branch_prefix}0"
-        f" - {args.output_repo}@{args.block_branch_prefix}{len(transformer_blocks)}"
+        f"Saving transformer blocks to {output_path}/0 - {output_path}/{len(transformer_blocks)}"
     )
 
     for i, block in enumerate(tqdm(transformer_blocks)):
-        #repo.git_checkout(args.client_branch, create_branch_ok=True)
-        #with repo.commit(
-        #    commit_message=args.commit_message, branch=args.block_branch_prefix + str(i), track_large_files=True
-        #):
-        path = os.path.join(args.output_path, f"pytorch_model_block_{i}.bin")
+        path = os.path.join(output_path, f"pytorch_model_block_{i}.bin")
         torch.save(block.state_dict(), path)
 
-    logger.info(f"Saving client-side modules to {args.output_repo}@{args.client_branch}")
+    logger.info(f"Saving client-side modules to {output_path}/client")
 
-    path = os.path.join(args.output_path, "client")
+    path = os.path.join(output_path, "client")
     os.makedirs(path, exist_ok=True)
 
-    #repo.git_checkout(args.client_branch, create_branch_ok=True)
-    #with repo.commit(commit_message=args.commit_message, branch=args.client_branch, track_large_files=True):
     model.decoder.layers = nn.ModuleList()
     model.save_pretrained(path)
     tokenizer.save_pretrained(path)
     config.save_pretrained(path)
-    logger.info(f"Converted {args.model} and pushed to {args.output_repo}")
+    logger.info(f"Converted {args.model} and stored to {output_path}")
 
 
 if __name__ == "__main__":
